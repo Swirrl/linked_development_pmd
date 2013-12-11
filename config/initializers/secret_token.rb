c21effa87ba5aefb6af5e35446a19fabcc113b6f
Rick Moynihan <rick.m@swirrl.com>
2013-12-11 14:48:46 +0000

    stock rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LinkedDevelopmentPmd::Application.config.secret_token = '2199710ad4ad47cd74145f17dd9bc7552a0832aafb43996ab53eacb9bc1e0b5db817daee63c00a980bfa0635cfd9a0103b6b324fc33bb5928afe860d472b1441'
