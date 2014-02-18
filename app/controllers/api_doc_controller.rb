c2b6f35a4d57494c804262d120329147eb77c102
Rick Moynihan <rick.m@swirrl.com>
2014-02-18 11:02:13 +0000

    Written basic api documentation and bodged it in.
    Formatting is currently broken as it's not properly integrated with PMD.
    
    Will tidy it up as I move it to the next version of PMD.

---
+++ app/controllers/api_doc_controller.rb
@@ -0,0 +1,6 @@
+require_dependency "publish_my_data/application_controller"
+
+class ApiDocController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 LinkedDevelopmentPmd::Application.routes.draw do
 
   match "/" => redirect('/data'), :as => 'home' # don't call this root - having two routes confuses devise (there's one in pmd_enterprise too).
 
+  scope '/api' do
+    get '/docs', to: 'api_doc#index', as: :api_docs
+  end
+
   mount PublishMyDataEnterprise::Engine, at: "/" , as: 'publish_my_data_enterprise'
   mount PublishMyData::Engine, at: "/"
+  
 end
