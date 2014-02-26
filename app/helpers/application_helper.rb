ae1f2f9956a31d9eb780792d9e377166b98fa769
G.J.Hilton <guyhilton@gjh.local>
2014-02-26 11:25:50 +0000

    menu highlighting restored

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,12 @@ module ApplicationHelper
   def cabi_menu_documentation
     overrides = {
                  :target => main_app.linked_development_api_docs_path,
-                 :items => [{
-                             title: "Linked Development API",
-                             target: main_app.linked_development_api_docs_path,
-                             highlight: nil
-                            },
+                 :highlight => "linkeddocumentation",
+                 :items => [
                             {
                              title: "Linked Data API",
                              target: publish_my_data.api_docs_path,
-                             highlight: nil
+                             highlight: "documentation"
                             }]
                 }
     
