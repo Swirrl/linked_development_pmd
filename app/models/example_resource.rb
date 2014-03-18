7088563d8584f3cc6c95f6237b417fbfe686f152
Rick Moynihan <rick.m@swirrl.com>
2014-03-18 17:36:59 +0000

    Better multi-lingual labels on sample data.

--- app/models/example_resource.rb
+++ app/models/example_resource.rb
@@ -5,7 +5,16 @@ class ExampleResource
   field :title, RDF::DC.title
   field :skos_literal_form, RDF::SKOSXL.literalForm
 
+  field :name_official, RDF::URI.new("http://www.fao.org/countryprofiles/geoinfo/geopolitical/resource/nameOfficial"), multivalued: true
+
   def label
-    rdfs_label || title || skos_literal_form || 'Sample Resource'
+    label = rdfs_label || title || skos_literal_form || name_official
+
+    labels = if label.class == Array
+               label.empty? ? ["Sample Resource"] : label
+             else
+               [label]
+             end
+    labels
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,20 +38,20 @@ PublishMyData.configure do |config|
 
   config.tripod_cache_store =  nil
   config.sparql_timeout_seconds = 25
+  config.response_limit_bytes = 5.megabytes
   config.application_name = "Linked Development"
-end
-
-Tripod.configure do |config|
-  config.update_endpoint = 'http://127.0.0.1:3030/linkeddev-dev/update'
-  config.data_endpoint = 'http://127.0.0.1:3030/linkeddev-dev/data'
-  config.response_limit_bytes = 10.megabytes
 
   # e.g memcached -m 1024 -p 11214 -I 5M -u memcache -l 127.0.0.1
-  config.cache_store  = Tripod::CacheStores::MemcachedCacheStore.new('localhost:11211')
+  config.tripod_cache_store  = nil #Tripod::CacheStores::MemcachedCacheStore.new('localhost:11211')
 end
 
 PublishMyDataEnterprise.configure do |c|
   c.aws_access_key_id = 'AKIAIR7BREBM76EWA2WQ'
   c.aws_secret_access_key = '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'
   c.downloads_s3_bucket = 'linkeddev-dumps'
+end
+
+Tripod.configure do |config|
+  config.update_endpoint = 'http://127.0.0.1:3030/linkeddev-dev/update'
+  config.data_endpoint = 'http://127.0.0.1:3030/linkeddev-dev/data'
 end
